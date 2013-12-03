212fec7a198e391c1dce8983a6670bbd5f88da40
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2013-12-03 11:27:58 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+The task
+---
+http://www.mimuw.edu.pl/~mengel/SO/PUBLIC-SO/11_zadanie/zad2.html
---
+++ serwer.c
@@ -0,0 +1,4 @@
+/* Tomasz Zakrzewski, nr indeksu: 336079 /
+/  SO2013 - second task			*/
+
+//TODO: EVERYTHING
