83fe07567da88b4cf05acac3c7b10854165f63ec
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2013-12-23 19:52:20 +0100

    A version with safe cleanup

--- serwer.c
+++ serwer.c
@@ -8,7 +8,6 @@
 #include "common.h"
 #include "err.h"
 
-
 #define MAX_K 99
 
 
@@ -27,7 +26,11 @@ Resource resources[MAX_K];
 Resource waitingWith[MAX_K];
 pid_t waitingPID[MAX_K];
 pthread_cond_t resourceCond[MAX_K];
+pthread_mutex_t finishMutex;
+pthread_cond_t finishCond;
 
+unsigned char interrupted = 0;
+unsigned int aliveThreads = 0;
 
 void lockMutex(const int k) {
 	int err;
@@ -36,10 +39,48 @@ void lockMutex(const int k) {
 }
 
 
+void lockFinishMutex() {
+	int err;
+	if ((err = pthread_mutex_lock(&finishMutex)) != 0)
+		syserr("Błąd przy zajmowaniu finishMuteksa: %d", err);
+}
+
+
+void unlockFinishMutex() {
+	int err;
+	if ((err = pthread_mutex_unlock(&finishMutex)) != 0)
+		syserr("Błąd przy zwalnianiu finishMuteksa: %d", err);
+}
+
+
 void unlockMutex(const int k) {
 	int err;
 	if ((err = pthread_mutex_unlock(&mutex[k])) != 0)
 		syserr("Błąd przy zwalnianiu muteksa[%d]: %d", k, err);
+}
+
+
+void increaseAlive() {
+	lockFinishMutex();
+	++aliveThreads;
+	unlockFinishMutex();
+}
+
+
+void decreaseAlive() {
+	int err;
+	if (debug)
+		fprintf(stderr, "Usiłuję skończyć wątek\n");
+	lockFinishMutex();
+	--aliveThreads;
+	if (debug)
+		fprintf(stderr, "Kończę wątek! Alive = %d\n", aliveThreads);
+	if (aliveThreads == 0) {
+		unlockFinishMutex();
+		if ((err = pthread_cond_signal(&finishCond)) != 0)
+			syserr("Błąd przy signal na finishCond: %d", err);
+	}
+	unlockFinishMutex();
 }
 
 
@@ -54,6 +95,11 @@ void* clientHandler(void* data) {
 	if (debug)
 		fprintf(stderr, "Wewnątrz nowego wątku: pid = %d k = %d n = %d\n", pid, k, n);
 
+	if (interrupted)
+		return (void*) 0;
+
+	increaseAlive();
+
 	lockMutex(k);
 
 	if (waitingWith[k]) {	//we have a process to be paired with
@@ -61,18 +107,26 @@ void* clientHandler(void* data) {
 		otherPID = waitingPID[k];
 		waitingWith[k] = 0;
 
-		while (otherN + n > resources[k]) {
+		while (!interrupted && otherN + n > resources[k]) {
 			if (debug)
 				fprintf(stderr, "PID %d czeka na %d zasobów typu %d\n", pid, waitingWith[k] + n, k);
 			if ((err = pthread_cond_wait(&resourceCond[k], &mutex[k])) != 0)
 				syserr("Błąd przy wait na resourceCond[%d]: %d", k, err);
-			if (otherN + n > resources[k]) { //we might have caught a wakeup that is not enough for us but is ok for somebody else
+			if (otherN + n > resources[k] || interrupted) { //we might have caught a wakeup that is not enough for us but is ok for somebody else
 				unlockMutex(k);
 				if ((err = pthread_cond_signal(&resourceCond[k])) != 0)
 					syserr("Błąd przy signal na resourceCond[%d]: %d", k, err);
 				lockMutex(k);
 			}
+		}
 
+		if (interrupted) {
+			if (debug)
+				fprintf(stderr, "int\n");
+			unlockMutex(k);
+			decreaseAlive();
+
+			return (void*) 0;
 		}
 
 		resources[k] -= n + otherN;
@@ -87,10 +141,9 @@ void* clientHandler(void* data) {
 		if (debug)
 			fprintf(stderr, "PID %d czeka na partnera typu %d (wychodzę)\n", pid, k);
 		unlockMutex(k);
+		decreaseAlive();
 		return (void*) 0;
 	}
-
-
 
 	if (debug)
 		fprintf(stderr, "PID %d, %d: gotowy!\n", pid, otherPID);
@@ -110,6 +163,9 @@ void* clientHandler(void* data) {
 	if (msgsnd(ipcOut, (char*) &msg, 2, 0) != 0)
 		syserr("Błąd przy informowaniu (zwalnianiu) klienta!");
 
+	if (debug)
+		fprintf(stderr, "%d i %d wysłały komunikaty.\n", pid, otherPID);
+
 	if (msgrcv(ipcIn, &msg, BUF_SIZE, pid, 0) == 0)
 		syserr("Otrzymano pustą odpowiedź od klienta!\n");
 
@@ -126,6 +182,8 @@ void* clientHandler(void* data) {
 	unlockMutex(k);
 	if ((err = pthread_cond_signal(&resourceCond[k])) != 0)
 		syserr("Błąd przy signal na resourceCond[%d]: %d", k, err);
+
+	decreaseAlive();
 
 	return (void *) 0;
 }
@@ -154,9 +212,48 @@ void cleanup() {
 }
 
 
-void exitServer(int sig) {
-	//cleanup();
-	exit(0);
+void dummyHandler(int sig) {
+	//noop
+}
+
+
+//a thread whose sole purpose is to exist and catch possible signals
+void* signalThread(void* data) {
+	sigset_t block_mask;
+	int sig, err, i;
+
+	if (signal(SIGINT, &dummyHandler) == SIG_ERR)
+		syserr("Signal");
+
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask, SIGINT);
+	sigwait(&block_mask, &sig);	//wait for the signal
+	fprintf(stderr, "Dostałem sygnał!\n");
+	interrupted = 1;
+
+	//wake up main if there is a need to:
+	struct Message msg;
+	msg.mtype = 1L;
+
+	for (int j = 0; j < n; ++j)
+		for (i = 0; i < k; ++i)	//wakey!
+			if ((err = pthread_cond_signal(&resourceCond[i])) != 0)
+				syserr("Błąd przy signal na resourceCond[%d]: %d", i, err);
+
+	lockFinishMutex();
+
+	if (debug)
+		fprintf(stderr, "signalThread wiesza się na cond (alive = %d)\n", aliveThreads);
+
+	while (aliveThreads > 0)
+		if ((err = pthread_cond_wait(&finishCond, &finishMutex)) != 0)
+			syserr("Błąd przy wait na finishCond: %d", err);
+
+	unlockFinishMutex();
+
+	msgsnd(ipcIn, (char*) &msg, 2, IPC_NOWAIT);
+
+	return (void*) 0;
 }
 
 
@@ -168,6 +265,11 @@ void initResources() {
 
 	if ((ipcOut = msgget(KEY_OUT, 0666 | IPC_CREAT)) == -1)
 		syserr("Nie można otworzyć kolejki IPC o id %ld", KEY_OUT);
+
+	if ((err = pthread_mutex_init(&finishMutex, 0)) != 0)
+		syserr("Błąd przy inicjowaniu finishMuteksa]: %d", err);
+	if ((err = pthread_cond_init(&finishCond, 0)) != 0)
+		syserr("Error przy inicjowaniu finishCond: %d", err);
 
 	for (i = 0; i < k; ++i) {
 		resources[i] = n;
@@ -182,6 +284,7 @@ void initResources() {
 int main(const int argc, const char** argv) {
 	int len = 0;
 	pthread_t th;
+	pthread_t cl;
 	pthread_attr_t attr;
 	int err;
 
@@ -189,22 +292,36 @@ int main(const int argc, const char** argv) {
 		syserr("Błędne użycie! Poprawna składnia to: ./serwer K N");
 
 	initResources();
-	atexit(cleanup);
+	//atexit(cleanup);
 
-	if (signal(SIGINT, exitServer) == SIG_ERR)
-		syserr("Błąd przy wywołaniu signal");
+	if ((err = pthread_attr_init(&attr)) != 0 )
+		syserr("Błąd przy attrinit: %d", err);
+
+	if ((err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE)) != 0)
+		syserr("Błąd przy setdetachstate: %d", err);
+
+	if ((err = pthread_create(&cl, &attr, &signalThread, info)) != 0)
+		syserr("Błąd przy create: %d", err);
+
+	if ((err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)) != 0)
+		syserr("Błąd przy setdetachstate: %d", err);
+
+	sigset_t set;
+	sigfillset(&set);
+	//sigaddset(&set, SIGINT);
+	sigprocmask(SIG_BLOCK, &set, NULL);
 
 	struct Message msg;
 
-	while (1) {
-		if ((len = msgrcv(ipcIn, &msg, BUF_SIZE, 1L, 0)) == 0) {
+	while (interrupted == 0) {
+		if (interrupted == 1 || (len = msgrcv(ipcIn, &msg, BUF_SIZE, 1L, 0)) == 0) {
 			if (debug)
 				fprintf(stderr, "Ostrzeżenie: otrzymano pustą wiadomość od klienta. Ignoruję...\n");
 			continue;
 		}
 
 		info = (struct ClientInfo*) malloc(sizeof(struct ClientInfo));
-		if (getClientInfo(msg.mtext, strlen(msg.mtext), info) == -1) {
+		if (interrupted == 1 || getClientInfo(msg.mtext, strlen(msg.mtext), info) == -1) {
 			if (debug)
 				fprintf(stderr, "Ostrzeżenie: otrzymano błędną wiadomość od klienta '%s'. Ignoruję...\n", msg.mtext);
 			continue;
@@ -213,17 +330,20 @@ int main(const int argc, const char** argv) {
 		if (debug)
 			fprintf(stderr, "Tworzę nowy wątek...\n");
 
-		if ((err = pthread_attr_init(&attr)) != 0 )
-			syserr("Błąd przy attrinit: %d", err);
-
-		if ((err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)) != 0)
-			syserr("Błąd przy setdetachstate: %d", err);
+		if (interrupted == 1)
+			break;
 
 		if ((err = pthread_create(&th, &attr, &clientHandler, info)) != 0)
 			syserr("Błąd przy create: %d", err);
 
 		info = NULL;
 	}
+
+	if (debug)
+		fprintf(stderr, "Main czeka na zakończenie wątków!\n");
+
+	pthread_join(cl, NULL);
+	cleanup();
 
 	return 0;
 }
